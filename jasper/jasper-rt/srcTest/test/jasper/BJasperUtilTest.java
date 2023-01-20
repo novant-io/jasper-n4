b0bdf4c66cea8094169a978295ed9297549d066b
Andy Frank <afrankvt@gmail.com>
2023-01-20 11:43:03 -0500

    Add BJasperUtilTest

---
+++ jasper/jasper-rt/srcTest/test/jasper/BJasperUtilTest.java
@@ -0,0 +1,63 @@
+//
+// Copyright (c) 2023, Andy Frank
+// Licensed under the MIT License
+//
+// History:
+//   20 Jan 2023  Andy Frank  Creation
+//
+
+package test.jasper;
+
+import java.io.*;
+import java.util.*;
+
+import javax.baja.nre.annotations.*;
+import javax.baja.sys.*;
+import javax.baja.test.BTestNg;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import jasper.util.*;
+
+/** BJasperUtilTest */
+@NiagaraType
+public class BJasperUtilTest extends BTestNg
+{
+
+/*+ ------------ BEGIN BAJA AUTO GENERATED CODE ------------ +*/
+
+////////////////////////////////////////////////////////////////
+// Type
+////////////////////////////////////////////////////////////////
+
+@Override
+public Type getType() { return TYPE; }
+public static final Type TYPE = Sys.loadType(BJasperUtilTest.class);
+
+/*+ ------------ END BAJA AUTO GENERATED CODE -------------- +*/
+
+  @BeforeMethod public void beforeMethod() {}
+  @AfterMethod public void afterMethod() {}
+
+////////////////////////////////////////////////////////////////
+// testSplitPath
+////////////////////////////////////////////////////////////////
+
+  @Test public void testSplitPath() throws IOException
+  {
+    verifyPath("/", new String[] {});
+    verifyPath("/foo", new String[] { "foo" });
+    verifyPath("/alpha/beta/gamma", new String[] { "alpha", "beta", "gamma" });
+  }
+
+  private void verifyPath(String path, String[] acc)
+  {
+    String[] test = JasperUtil.splitPath(path);
+    verifyEq(test.length, acc.length);
+    for (int i=0; i<test.length; i++)
+      verifyEq(test[i], acc[i]);
+  }
+}

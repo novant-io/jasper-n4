ea51773e2bf3b1ee6588fa5cf052936004c1e57e
Andy Frank <afrankvt@gmail.com>
2024-03-14 13:55:50 -0400

    Roll to 0.11 cycle

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ vendor {
   defaultVendor("Novant")
 
   // defaultModuleVersion sets the "vendorVersion" attribute on all modules
-  defaultModuleVersion("0.10")
+  defaultModuleVersion("0.11")
 }
 
 niagaraSigning {

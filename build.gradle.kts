0aeee9061117f673fba0cbca6261d1b7eecd4cbc
Andy Frank <afrankvt@gmail.com>
2024-11-29 12:20:02 -0500

    Roll to 0.17 cycle

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ vendor {
   defaultVendor("Novant")
 
   // defaultModuleVersion sets the "vendorVersion" attribute on all modules
-  defaultModuleVersion("0.16")
+  defaultModuleVersion("0.17")
 }
 
 niagaraSigning {
--- changelog.md
+++ changelog.md
@@ -1,5 +1,7 @@
 # Changelog
 
+## Version 0.17 (working)
+
 ## Version 0.16 (29-Nov-2024)
 * Support for new `/write` endpoint
 
